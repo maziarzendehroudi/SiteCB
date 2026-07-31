d27229506fe471808af2e20fa17d354581e11d7e
maziarzendehroudi <maziarzendehroudi@users.noreply.github.com>
2026-07-31 13:59:49 +0000

    deploy: b423d7026c22f5914c3f41da0652c64b2071b11e

--- assets/home-KV0GaLjU.js
+++ assets/home-KV0GaLjU.js
@@ -1,5 +1,5 @@
 var e=`---
-title: "Accueil - Camille Bongue2"
+title: "Accueil - Camille Bongue"
 slug: "home"
 ---
 
@@ -7,7 +7,7 @@ slug: "home"
 
 <section class="text-section">
     <div class="container">
-        <h1 class="main-title text-center" style="font-size: 2.2rem; margin-bottom: 2rem;">Bienvenue au cabinet de psychothérapie</h1>
+        <h1 class="main-title text-center" style="font-size: 2.2rem; margin-bottom: 2rem;">ça marche !</h1>
         <p>Le cabinet se situe au sein du <strong>Centre médical Adamantium</strong>, à Cap Alpha, à Clapiers (Zone Grand Montpellier).</p>
         <p>Offrant un espace calme favorisant la sérénité et le respect de votre démarche, les consultations se font exclusivement sur rendez-vous.</p>
         <a href="#contact" class="cta-button" onclick="document.querySelector('a[href=\\\\'#contact\\\\']').click(); return false;">Prendre rendez-vous</a>
